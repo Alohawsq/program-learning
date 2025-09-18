7a62f883b5b7ff72e0623448a9005bc6b9f5963c
王淑庆 <wangshuqing@datagrand.com>
2025-09-18 08:56:04 +0800

    update 2349

--- base_learn/other/2349-设计数字容器系统.py
+++ base_learn/other/2349-设计数字容器系统.py
@@ -8,22 +8,47 @@ void change(int index, int number) 在下标 index 处填入 number 。如果该
 int find(int number) 返回给定数字 number 在系统中的最小下标。如果系统中没有 number ，那么返回 -1 。
 
 """
+import heapq
 
 class NumberContainers:
 
     def __init__(self):
-        ...
+        self.index_to_num = {}
+        self.num_to_heap = {}
+
     def change(self, index: int, number: int) -> None:
-        ...
+        if index in self.index_to_num and self.index_to_num[index] == number:
+            return
+
+        self.index_to_num[index] = number
+
+        if number not in self.num_to_heap:
+            self.num_to_heap[number] = []
+        heapq.heappush(self.num_to_heap[number], index)
 
     def find(self, number: int) -> int:
-        ...
+        if number not in self.num_to_heap:
+            return -1
 
+        heap = self.num_to_heap[number]
+        while heap and self.index_to_num.get(heap[0]) != number:
+            heapq.heappop(heap)
+
+        return heap[0] if heap else -1
 
 
 if __name__=='__main__':
     number = ["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"]
     index = [[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]]
     obj = NumberContainers()
-    obj.change(index, number)
-    param_2 = obj.find(number)
+    output = []
+    for i, op in enumerate(number):
+        if op == "find":
+            result = obj.find(index[i][0])
+            output.append(result) if result else output.append(None)
+        elif op == "change":
+            result = obj.change(index[i][0], index[i][1])
+            output.append(result) if result else output.append(None)
+        else:
+            output.append(None)
+    print(output)
