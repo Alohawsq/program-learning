9c52b2d11902df64530200acad0bb3a7adef94e7
王淑庆 <wangshuqing@datagrand.com>
2025-09-18 11:15:01 +0800

    update 3408

--- base_learn/other/3408-设计任务管理器.py
+++ base_learn/other/3408-设计任务管理器.py
@@ -15,23 +15,78 @@ from typing import List
 class TaskManager:
 
     def __init__(self, tasks: List[List[int]]):
-        ...
+        self.priority_to_taskId = {}
+        self.taskId_info = {}
+        for task in tasks:
+            if task[2] not in self.priority_to_taskId:
+                self.priority_to_taskId[task[2]] = [task[1]]
+            else:
+                self.priority_to_taskId[task[2]].append(task[1])
+            # 记录task_id与userId和priority的关系
+            self.taskId_info[task[1]] = [task[0], task[2]]
     def add(self, userId: int, taskId: int, priority: int) -> None:
-        ...
+        self.taskId_info[taskId] = [userId, priority]
+        if priority not in self.priority_to_taskId:
+            self.priority_to_taskId[priority] = [taskId]
+        else:
+            self.priority_to_taskId[priority].append(taskId)
+
     def edit(self, taskId: int, newPriority: int) -> None:
-        ...
+        last_priority = self.taskId_info[taskId][1]
+        self.taskId_info[taskId][1] = newPriority
+        self.priority_to_taskId[last_priority].remove(taskId)
+        if not self.priority_to_taskId[last_priority]:
+            del self.priority_to_taskId[last_priority]
+        if newPriority not in self.priority_to_taskId:
+            self.priority_to_taskId[newPriority] = [taskId]
+        else:
+            self.priority_to_taskId[newPriority].append(taskId)
 
     def rmv(self, taskId: int) -> None:
-        ...
+        last_priority = self.taskId_info[taskId][1]
+        del self.taskId_info[taskId]
+        self.priority_to_taskId[last_priority].remove(taskId)
+        if not self.priority_to_taskId[last_priority]:
+            del self.priority_to_taskId[last_priority]
 
     def execTop(self) -> int:
-        ...
+        if not self.priority_to_taskId:
+            return -1
+        max_priority = max(list(self.priority_to_taskId.keys()))
+        print(self.priority_to_taskId)
+        max_task_id = max(self.priority_to_taskId[max_priority])
+        userId = self.taskId_info[max_task_id][0]
+        del self.taskId_info[max_task_id]
+        self.priority_to_taskId[max_priority].remove(max_task_id)
+        if not self.priority_to_taskId[max_priority]:
+            del self.priority_to_taskId[max_priority]
+        return userId
+
 
 if __name__=='__main__':
-    tasks = ["TaskManager","add","edit","execTop","rmv","add","execTop"]
+    tasks = ["TaskManager", "add", "edit", "execTop", "rmv", "add", "execTop"]
     items = [[[[1, 101, 10], [2, 102, 20], [3, 103, 15]]], [4, 104, 5], [102, 8], [], [101], [5, 105, 15], []]
-    obj = TaskManager(tasks)
-    obj.add(userId,taskId,priority)
-    obj.edit(taskId,newPriority)
-    obj.rmv(taskId)
-    param_4 = obj.execTop()
+    tasks = ["TaskManager", "add", "edit", "execTop", "rmv", "add", "execTop"]
+    items = [[[[1, 101, 8], [2, 102, 20], [3, 103, 5]]], [4, 104, 5], [102, 9], [], [101], [50, 101, 8], []]
+    tasks = ["TaskManager", "rmv", "execTop"]
+    items = [[[[10, 26, 25]]], [26], []]
+    obj = None
+    output = []
+    for index, op in enumerate(tasks):
+        if op == "TaskManager":
+            obj = TaskManager(items[index][0])
+            output.append(None)
+        elif op == "add":
+            obj.add(items[index][0], items[index][1], items[index][2])
+            output.append(None)
+        elif op == "edit":
+            obj.edit(items[index][0], items[index][1])
+            output.append(None)
+        elif op == "execTop":
+            result = obj.execTop()
+            output.append(result) if result else output.append(None)
+        elif op == "rmv":
+            obj.rmv(items[index][0])
+            output.append(None)
+    print(f"***** output *****: {output}")
+
